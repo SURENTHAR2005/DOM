1c0f8e18450cb3b903ad07b73f1e561a6a88d055
SURENTHAR2005 <surentharvb2005@gmail.com>
2025-06-20 15:19:20 +0530

    catt project completed

--- cartquantity/script.js
+++ cartquantity/script.js
@@ -4,8 +4,31 @@ if (!cartQuantity) {
     localStorage.setItem("cartQuantity", 0);
     cartQuantity = 0;
 }
+const display=document.getElementById("displaybutton");
+const plus1=document.getElementById("plus1button");
+const plus2=document.getElementById("plus2button");
+const plus3=document.getElementById("plus3button");
+const reset=document.getElementById("resetbutton");
 
-const plus1
+display.addEventListener("click",()=>{
+    displayQuantity();
+});
+plus1.addEventListener("click",()=>{
+    updateQuantity(1);
+    displayQuantity();
+});
+plus2.addEventListener("click",()=>{
+    updateQuantity(2);
+    displayQuantity();
+});
+plus3.addEventListener("click",()=>{
+    updateQuantity(3);
+    displayQuantity();
+});
+reset.addEventListener("click",()=>{
+    resetCart();
+    displayQuantity();
+});
 
 function updateQuantity(value) {
     cartQuantity=cartQuantity + value;
@@ -16,7 +39,7 @@ function displayQuantity() {
     console.log("cart : " + cartQuantity);
 
 }
-function resetcart() {
+function resetCart() {
     cartQuantity = 0;
     console.log("the values has been reset");
     localStorage.setItem("cartQuantity",cartQuantity);
