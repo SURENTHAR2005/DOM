7a57584641a626f1aba394d71e30f8d30afb49ac
SURENTHAR2005 <surentharvb2005@gmail.com>
2025-06-20 10:27:18 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+// newObject={
+//     display:function(){
+//         console.log("hi");
+//     }
+// }
+// console.log(newObject.display());
+
+//dom
+console.log("hi")
+console.warn("warning")
+console.error("Danger error") 
+console.table([{ name: "surenthar v b", age: 33 }])
+console.time("loop")
+for(let i=0;i<100000000;i++){ }
+console.timeEnd("loop")
+
+//eval() function
+const x=10;
+const y=20;
+const result=eval("x+ y")
+console.log(result);
+
+
+//use strict mode
+"use strict"
+let a=90;
+b=70;
+console.log(b);
