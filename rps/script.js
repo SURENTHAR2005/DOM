67f07be67a562504dbf0464a1bff630354ba78d1
SURENTHAR2005 <surentharvb2005@gmail.com>
2025-06-20 21:34:38 +0530

    completed rock paper sissor

--- rps/script.js
+++ rps/script.js
@@ -1,81 +1,95 @@
 //user move
-let rock=document.getElementById("rock-B");
-let paper=document.getElementById("paper-B");
-let sissor=document.getElementById("sissor-B");
-
-
+let rock = document.getElementById("rock-B");
+let paper = document.getElementById("paper-B");
+let sissor = document.getElementById("sissor-B");
 
 //computer move
-let randomVal=Math.random();
-let computerMove;
-if(randomVal>0 && randomVal<(1/3)){
-    computerMove="ROCK";
+function getcomp() {
+    let randomVal = Math.random();
+    if (randomVal > 0 && randomVal < (1 / 3)) {
+        return "ROCK";
 
+    }
+    else if (randomVal > 1 / 3 && randomVal < (2 / 3)) {
+        return "PAPER";
+    }
+    else {
+        return "SISSOR";
+    }
 }
-else if(randomVal>1/3 && randomVal<(2/3)){
-    computerMove="PAPER";
-}
-else{
-    computerMove="SISSOR";
-}
-//console.log(computerMove);
+
+//user move
 let userMove;
-rock.addEventListener("click",()=>{
-    userMove="ROCK";
-    rockfunc(userMove,computerMove);
+rock.addEventListener("click", () => {
+    userMove = "ROCK";
+    let computerMove = getcomp();
+    rockfunc(userMove, computerMove);
 });
 
-paper.addEventListener("click",()=>{
-    userMove="PAPER";
-    paperfunc(userMove,computerMove);
+paper.addEventListener("click", () => {
+    userMove = "PAPER";
+    let computerMove = getcomp();
+    paperfunc(userMove, computerMove);
 });
 
-sissor.addEventListener("click",()=>{
-    userMove="SISSOR";
-    sissorfunc(userMove,computerMove);
+sissor.addEventListener("click", () => {
+    userMove = "SISSOR";
+    let computerMove = getcomp();
+    sissorfunc(userMove, computerMove);
 });
 
 
-let result=document.querySelector(".result-B");
-let userpoint=0;
-let computerpoint=0;
+let result = document.querySelector(".result-B");
+let userpoint = 0;
+let computerpoint = 0;
+
 //function
-function rockfunc(userMove,computerMove){
+function rockfunc(userMove, computerMove) {
     console.log("rockfunc");
-    if(userMove===computerMove){
-        result.innerText="MATCH DRAW";
-        
+    if (userMove === computerMove) {
+        result.innerText = "MATCH DRAW";
 
-    }else if(userMove==="ROCK" && computerMove==="SISSOR"){
-        result.innerText="YOU WON";
+
+    } else if (userMove === "ROCK" && computerMove === "SISSOR") {
+        result.innerText = "YOU WON";
         userpoint++;
     }
-    else{
-        result.innerText="YOU LOOSE";
+    else {
+        result.innerText = "YOU LOOSE";
         computerpoint++;
     }
-    console.log(userpoint,computerpoint);
-    
+    console.log(userpoint, computerpoint);
+    localStorage.setItem("USER-POINT", userpoint);
+    localStorage.setItem("COMPUTER-POINT", computerpoint);
 }
-function paperfunc(userMove,computerMove){
-    if(userMove===computerMove){
-        result.innerText="MATCH DRAW";
-    }else if(userMove==="PAPER" && computerMove==="ROCK"){
-        result.innerText="YOU WON";
+
+function paperfunc(userMove, computerMove) {
+    if (userMove === computerMove) {
+        result.innerText = "MATCH DRAW";
+    } else if (userMove === "PAPER" && computerMove === "ROCK") {
+        result.innerText = "YOU WON";
         userpoint++;
-    }else{
-        result.innerText="YOU LOOSE";
+    } else {
+        result.innerText = "YOU LOOSE";
         computerpoint++;
     }
+    console.log(userpoint, computerpoint);
+    localStorage.setItem("USER-POINT", userpoint);
+    localStorage.setItem("COMPUTER-POINT", computerpoint);
 }
-function sissorfunc(userMove,computerMove){
-    if(userMove===computerMove){
-        result.innerText="MATCH DRAW";
-    }else if(userMove==="SISSOR" && computerMove==="PAPER"){
-        result.innerText="YOU WON";
+
+function sissorfunc(userMove, computerMove) {
+    if (userMove === computerMove) {
+        result.innerText = "MATCH DRAW";
+    } else if (userMove === "SISSOR" && computerMove === "PAPER") {
+        result.innerText = "YOU WON";
         userpoint++;
-    }else{
-        result.innerText="YOU LOOSE";
+    } else {
+        result.innerText = "YOU LOOSE";
         computerpoint++;
     }
+    console.log(userpoint, computerpoint);
+    localStorage.setItem("USER-POINT", userpoint);
+    localStorage.setItem("COMPUTER-POINT", computerpoint);
 }
+
