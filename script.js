17a53f7674699a6fb03fb856765866184b4efd4c
SURENTHAR2005 <surentharvb2005@gmail.com>
2025-06-20 13:49:29 +0530

    default number

--- script.js
+++ script.js
@@ -6,23 +6,39 @@
 // console.log(newObject.display());
 
 //dom
-console.log("hi")
-console.warn("warning")
-console.error("Danger error") 
-console.table([{ name: "surenthar v b", age: 33 }]);
-console.time("loop")
-for(let i=0;i<1000000000000;i++){ }
-console.timeEnd("loop")
+// console.log("hi")
+// console.warn("warning")
+// console.error("Danger error") 
+// console.table([{ name: "surenthar v b", age: 33 }]);
+// console.time("loop")
+// for(let i=0;i<1000000000000;i++){ }
+// console.timeEnd("loop")
 
-//eval() function
-const x=10;
-const y=20;
-const result=eval("x+ y")
-console.log(result);
+// //eval() function
+// const x=10;
+// const y=20;
+// const result=eval("x+ y")
+// console.log(result);
 
 
-//use strict mode
-"use strict"
-let a=90;
-b=70;
-console.log(b);
+// //use strict mode
+// "use strict"
+// let a=90;
+// b=70;
+// console.log(b);
+
+
+// ternary operator
+// condition ? if_logic:else_logic;
+// let gratestvalue=(2>3)?1:2;
+// console.log(gratestvalue);
+
+//gaurd operator
+let a=(1>2) && 5; //when condition become true 5 assigned to a
+
+
+//default operator
+let input;
+let b=input||4;//if input contains falsy numbers it returns 4 
+
+//falsy values-0,numm," ",undefined
